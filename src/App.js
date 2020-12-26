6c459446ba0bfe63225550c3ef84f25b47813d80
cuongcuoi <cuongmk288@gmail.com>
2020-12-27 00:20:48 +0700

    App-chat

--- src/App.js
+++ src/App.js
@@ -1,73 +1,203 @@
-import React, {useState, useEffect,useRef} from 'react'
-import { MyGlobalContext } from './unit-22/MyGlobalContext'
-import UserList from './unit-23/UserList'
-import axios from 'axios'
-import 'antd/dist/antd.css';
-import TestUseMemo from './unit-23/TestUseMemo'
-import { css } from "@emotion/core";
-import ClipLoader from "react-spinners/ClipLoader";
-import MyTable from './unit-23/Table'
+// import React, {useState, useEffect,useRef} from 'react'
+// import { MyGlobalContext } from './unit-22/MyGlobalContext'
+// import UserList from './unit-23/UserList'
+// import axios from 'axios'
+// import 'antd/dist/antd.css';
+// import TestUseMemo from './unit-23/TestUseMemo'
+// import { css } from "@emotion/core";
+// import ClipLoader from "react-spinners/ClipLoader";
+// import MyTable from './unit-23/Table'
+
+// function App() {
+// //   const h1 = useRef(null)
+// //   const [loading, setLoading] = useState(true)
+
+//   const [gContext, setGContext] = useState({
+//     user: null
+//   })
+
+// //   //Css loading
+// //   const loadingCss = css`
+// //   display: block;             
+// //   border-color: red;
+// // `;
+
+// //   const getCurrentUser = id => {
+// //     axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+// //       .then(response => {
+// //         // console.log(response)
+// //         const user = response.data
+// //         setGContext({
+// //           user
+// //         })
+// //         setLoading(false)
+// //       })
+// //   }
+
+// //   useEffect(()=>{
+// //     getCurrentUser(2)
+// //   }, [])
+
+//   // useEffect(()=>{
+//   //   console.log(h1)
+
+//   // }, [h1])
+
+//   return (
+//     <MyGlobalContext.Provider value={gContext}>
+//       {/* {
+//         !loading && 
+//         <UserList />
+//       }
+//       <ClipLoader
+//         css={loadingCss}
+//         size={150}
+//         color={"#123abc"}
+//         loading={loading}
+//       />
+
+//         <h1 ref={h1}>Buoi 23</h1>
+//         {
+//            gContext.user && <UserList /> 
+//         }
+//         <h1 ref={h1}>Buoi 24</h1> */}
+
+//        {/* <TestUseMemo/> */}
+
+//         <MyTable/>
+//     </MyGlobalContext.Provider>
+    
+//   );
+// }
+
+// export default App;
+
+import React, { useRef, useState } from 'react';
+import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDJOlh0CccmYcFSORIyZX5gzYzRPWwc7kE",
+  authDomain: "reactjs-5040e.firebaseapp.com",
+  projectId: "reactjs-5040e",
+  storageBucket: "reactjs-5040e.appspot.com",
+  messagingSenderId: "595289870911",
+  appId: "1:595289870911:web:df5c7fb4186d9862cbde8a",
+  measurementId: "G-CXX99N8J4H"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 
 function App() {
-//   const h1 = useRef(null)
-//   const [loading, setLoading] = useState(true)
 
-  const [gContext, setGContext] = useState({
-    user: null
-  })
-
-//   //Css loading
-//   const loadingCss = css`
-//   display: block;             
-//   border-color: red;
-// `;
-
-//   const getCurrentUser = id => {
-//     axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
-//       .then(response => {
-//         // console.log(response)
-//         const user = response.data
-//         setGContext({
-//           user
-//         })
-//         setLoading(false)
-//       })
-//   }
-
-//   useEffect(()=>{
-//     getCurrentUser(2)
-//   }, [])
-
-  // useEffect(()=>{
-  //   console.log(h1)
-
-  // }, [h1])
+  const [user] = useAuthState(auth);
 
   return (
-    <MyGlobalContext.Provider value={gContext}>
-      {/* {
-        !loading && 
-        <UserList />
-      }
-      <ClipLoader
-        css={loadingCss}
-        size={150}
-        color={"#123abc"}
-        loading={loading}
-      />
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
 
-        <h1 ref={h1}>Buoi 23</h1>
-        {
-           gContext.user && <UserList /> 
-        }
-        <h1 ref={h1}>Buoi 24</h1> */}
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
 
-       {/* <TestUseMemo/> */}
-
-        <MyTable/>
-    </MyGlobalContext.Provider>
-    
+    </div>
   );
 }
+
+function SignIn() {
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
+}
+
 
 export default App;
--- src/unit-23/Table.js
+++ src/unit-23/Table.js
@@ -1,6 +1,14 @@
-import React, { useState } from 'react'
-import { Popconfirm, Table, Button, Form, Input } from 'antd'
+import React, { useState,useEffect } from 'react'
+import { Popconfirm, Table, Button, Form, Input, Select, Spin, } from 'antd'
 import { v4 as uuid } from 'uuid'
+import axios from 'axios'
+import { LoadingOutlined,SmileOutlined } from '@ant-design/icons';
+
+const SUBJECT_LIST = [
+  {id: 1,name: 'Reactjs'},
+  {id: 2,name: 'Nodejs'},
+  {id: 3,name: 'Vuejs'},
+]
 
 const initData = [
   {
@@ -8,25 +16,72 @@ const initData = [
     name: 'Edward King 0',
     age: '32',
     address: 'London, Park Lane no. 0',
+    subject: 1
   },
   {
     key: '1',
     name: 'Edward King 1',
     age: '32',
     address: 'London, Park Lane no. 1',
+    subject: 2
   },
 ]
 
-const EditableCell = ({children, editing, dataIndex}) => {
+const EditableCell = ({ children, editing, dataIndex, inputType,currentSelected,selectList,isLoadingSubjects }) => {
+  const isNormalMode = ['text', 'number','textarea', ].includes(inputType)
+  const isSelectedMode = inputType === 'select'
+  let NormalComponent = Input
+  if (inputType === 'textarea') {
+    NormalComponent = Input.TextArea
+  }
+
+  let renderEditing = null
+  if (isNormalMode) {
+    renderEditing = <NormalComponent type={inputType}/>
+  } else if (isSelectedMode) {
+    renderEditing = (
+      <Select>
+        {
+          selectList.map(item => {
+            return <Select.Option value={item.id} key={item.id}>{item.name}</Select.Option>
+          })
+        }
+      </Select>
+    )
+  }
+  
+  let renderReadOnly = children
+  if (isSelectedMode) {
+    if (isLoadingSubjects) {
+      renderReadOnly =  <SmileOutlined  style={{ fontSize: 30 }} spin />
+    } else {
+      const item = selectList.find(item => item.id === currentSelected)
+      // console.log('curent',currentSelected)
+      renderReadOnly = item ? item.name : ''
+    }
+    
+  }
+
+
+  const normalEdit = <NormalComponent type={inputType}/>
   return <td>
     {
       editing ? (
-        <Form.Item name={dataIndex}>
-          <Input />
+        <Form.Item name={dataIndex}
+        rules={
+          [
+            {
+              requires: true,
+              message: `nhap gi di pls${dataIndex}`
+            }
+          ]
+        }>
+          {renderEditing}
+          {/* <Input type={inputType} /> */}
         </Form.Item>
       ) : (
-        children
-      )
+        renderReadOnly
+        )
     }
   </td>
 }
@@ -35,6 +90,10 @@ const MyTable = () => {
   const [form] = Form.useForm()
   const [items, setItems] = useState(initData)
   const [editingKey, setEditingKey] = useState('')
+  const [subjects,setSubjects] = useState([
+    // {id:0,name:'Please choose a subject'}
+  ])
+  const [isLoadingSubjects, setIsLoadingSubjects] = useState(true)
 
   const handleDelete = key => {
     const newItems = items.filter(item => item.key !== key)
@@ -43,8 +102,43 @@ const MyTable = () => {
 
   const isEditing = (record) => record.key === editingKey;
 
-  const edit = record => {
+  const edit = (record) => {
+    form.setFieldsValue({
+      name: '',
+      age: '',
+      address: '',
+      ...record,
+    })
 
+    const values = form.getFieldValue()
+    // console.log(values)
+    setEditingKey(record.key);
+  }
+  const save =async key => {
+    const record = items.find(item => item.key === key)
+    if (!record) {
+      return
+    }
+    //validate
+    const validateRult =await form.validateFields()
+    console.log(validateRult)
+
+    //get form data
+    const value = form.getFieldValue()
+    if (Array.isArray(value)){
+      return
+    }
+    
+    // console.log(value)
+
+    //save data
+    const index = items.findIndex(item => item.key === key)
+    const item = items[index]
+
+    const newData = { ...item, ...value }
+    items[index] = newData
+    setItems([...items])
+    setEditingKey('')
   }
 
   const columns = [
@@ -53,14 +147,55 @@ const MyTable = () => {
       dataIndex: 'name',
       width: '30%',
       editable: true,
+      
+      onCell: (record, index) => {
+        return {
+          record,
+          inputType: 'text',
+          dataIndex: 'name',
+          editing: isEditing(record)
+        }
+      }
     },
     {
       title: 'age',
       dataIndex: 'age',
+      onCell: (record, index) => {
+        // console.log({record,index});
+        return {
+          record,
+          inputType: 'number',
+          dataIndex: 'age',
+          editing: isEditing(record)
+        }
+      }
     },
     {
       title: 'address',
       dataIndex: 'address',
+      onCell: (record, index) => {
+        return {
+          record,
+          inputType: 'textarea',
+          dataIndex: 'address',
+          editing: isEditing(record)
+        }
+      }
+    },
+    {
+      title: 'Subject',
+      dataIndex: 'subject',
+      onCell: (record, index) => {
+        return {
+          record,
+          inputType: 'select',
+          dataIndex: 'subject',
+          selectList:subjects,
+          isLoadingSubjects,
+          currentSelected: record.subject,
+          editing: isEditing(record)
+        }
+      }
     },
     {
       title: 'operation',
@@ -69,12 +204,19 @@ const MyTable = () => {
         if (!items.length) {
           return null
         }
-
         const editable = isEditing(record)
-
         return (<>
-          <Button onClick={() => edit(record)}>Edit</Button>
+          {
+            editable ? (
+              <>
+                <Button onClick={() => save(record.key)}>save</Button>
+                <Button onClick={() => setEditingKey('')}>cancel</Button>
+              </>
+            ) : (
+                <Button onClick={() => edit(record)}>Edit</Button>
+              )
 
+          }
           <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
             <a>Delete</a>
           </Popconfirm>
@@ -91,13 +233,25 @@ const MyTable = () => {
         name: '',
         age: '',
         address: '',
+        subject: '',
       }
     ])
   }
 
+  const fetchSubject = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todosw')
+      .then(() => {
+        setSubjects(SUBJECT_LIST)
+        setIsLoadingSubjects(false)
+      })
+  }
+
+  useEffect(()=>{
+    fetchSubject()
+  },[])
+
   return <>
     <Button onClick={addNewItem}>Add new</Button>
-
     <Form form={form} component={false}>
       <Table
         rowClassName={() => 'editable-row'}
